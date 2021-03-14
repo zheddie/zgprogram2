5c73728bd4df56e28adcb622d26dfca391d54e3b
GavinZhang <zhanggan@cn.ibm.com>
2021-03-14 20:53:56 +0800

    Init version of zgprogram2

---
+++ frontend/home.js
@@ -0,0 +1,140 @@
+function go(){
+        var searchtxt = document.getElementById('searchinput').value;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {displaySearchResult(this);}
+        };
+        xhttp.open("GET", "http://localhost:18701/notes?search="+searchtxt, true);
+        xhttp.send();
+}
+
+function addnew() {
+    
+    const title = document.getElementById('title').value;
+    if(title ==="" ) alert("Title can not be NULL");
+    else{
+        var markup = $('.notecontent').summernote('code');
+        const updateData = {"title":title, "content":markup};
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {doadd(this);}
+        };
+        xhttp.open("PUT", "http://localhost:18701/onenote", true);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(JSON.stringify(updateData));
+    }
+};
+function doadd(){
+    console.log("Add note done!?");
+    go();
+}
+
+function deleteNote(){
+    if(!document.currentID) alert("Not note was selected!");
+    else{
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {dodelete(this);}
+        };
+        xhttp.open("DELETE", "http://localhost:18701/onenote?id="+document.currentID, true);
+        xhttp.send();        
+    }
+};
+function dodelete(xhttp) {
+    console.log("deleteOneNote done!?");
+    go();
+};
+function save() {
+    var markup = $('.notecontent').summernote('code');
+    const title = document.getElementById('title').value;
+    // console.log(document.currentID);
+    // console.log(title);
+    // console.log(markup);
+    const updateData = {"title":title, "content":markup};
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {updateOneNote(this);}
+    };
+
+    xhttp.open("POST", "http://localhost:18701/onenote?id="+document.currentID, true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(updateData));
+    //$('.zclick2edit').summernote('destroy');
+};
+function updateOneNote(xhttp) {
+    console.log("updateOneNote done!?");
+    go();
+}
+
+function loadAllNotes() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {displaySearchResult(this);}
+    };
+    xhttp.open("GET", "http://localhost:18701/notes?search=all", true);
+    xhttp.send();
+}
+function displaySearchResult(xhttp) {
+    var notes = JSON.parse(xhttp.responseText);
+    var newContent = "<div class='booksGallery'><table>";
+    document.currentID ='';
+    notes.forEach(function(note) {
+        if(!document.currentID || document.currentID===''){
+            document.currentID = note.idntfr;
+        }
+        var processedTitle = note.title;
+        if (processedTitle.length > 35) { processedTitle = processedTitle.substring(0, 33) + "..."; } 
+        //newContent += `<tr><td><div class="gallery"><a target="_blank" href="${processedTitle}>"</a>`+
+        newContent += `<tr><td><div class="gallery">`+
+        // `<div id="id${note.idntfr}" onClick=getNoteDetails('${note.idntfr}') onMouseOver="onmouseover('${note.idntfr}')" class='galleryTitle'>${processedTitle}</div></div>`+
+        `<div id="id${note.idntfr}" onClick=getNoteDetails('${note.idntfr}') class='galleryTitle'>${processedTitle}</div></div>`+
+        `</td></tr>`;
+        // newContent += `<div class="gallery"><a target="_blank" href="${book.web_url}">` + 
+        //               `<img id="bookImage" src="${book.image_url}" width="600" height="400"></a>` + 
+        //               `<div onClick=getBookDetails('${book.title.replace(/\s/g, '%20')}') class='galleryTitle'>${processedTitle}</div></div>`;
+    })
+    newContent += "</table></div>";
+    document.getElementById("searchResult").innerHTML = newContent;
+    console.log("currentID:"+document.currentID);
+    var div = document.getElementById("id"+document.currentID);
+    div.style.backgroundColor = "green";
+    getNoteDetails(document.currentID);
+}
+function onmouseover(id){
+    console.log(id);
+    var div = document.getElementById("id"+id);
+    div.style.backgroundColor = "gray";
+    
+}
+function getNoteDetails(idntfr) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {displayNoteDetail(this);}
+    };
+    
+    xhttp.open("GET", `http://localhost:18701/onenote?id=${idntfr}`, true);
+    xhttp.send();
+}
+function displayNoteDetail(xhttp) {
+    var jsonData = JSON.parse(xhttp.responseText);
+    console.log(jsonData);
+    var noteTitle = jsonData[0]['title'];
+    var noteMain = jsonData[0]['content'];
+    if (document.currentID) {
+        var div = document.getElementById("id"+document.currentID);
+        div.style.backgroundColor = "white";
+    }
+    document.currentID = jsonData[0]['idntfr'];
+    var div = document.getElementById("id"+document.currentID);
+    div.style.backgroundColor = "green";
+    document.getElementById("title").value = noteTitle;
+    $('.notecontent').summernote("code",noteMain);
+    // var newContent = `<img id="bookImage" src="${imageUrl}"></a>`;
+    // newContent += `<div><label>Title:</label> <input id="titleInput" type="text" value='${bookTitle.replaceAll('%20', ' ')}' size='50'></input></div>`;
+    // newContent += `<div><label>Web URL:</label> <input id="webUrlInput" type="text" value='${webUrl}' size='50'></input></div>`;
+    // newContent += `<div><label>Image URL:</label> <input id="imageUrlInput" type="text" value='${imageUrl}' size='50'></input></div>`;
+    // newContent += `<div><button class="btn btn-success" onClick=updateBook('${bookTitle}') style="float:middle; margin-right: 50px;">Update</button>` + 
+    //               `<button class="btn btn-danger" style="float:middle" onClick=deleteBook('${bookTitle}')>Delete</button></div></div>`;
+    // document.getElementById("main").innerHTML = newContent;
+} 
+
---
+++ server.js
@@ -0,0 +1,138 @@
+const fs = require('fs')
+const mysql = require('mysql');
+var express = require('express');
+
+var app = express();
+app.use(express.json({limit: '25mb', extended: true}));
+app.use(express.urlencoded({limit: '25mb', extended: true}));
+app.use(express.json()); // JSON parser for post request
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // For POST CORS Error
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+    next();
+});
+// var bodyParser = {
+//     json: {limit: '50mb', extended: true},
+//     urlencoded: {limit: '50mb', extended: true}
+//   };
+
+//setup mysql
+const MYSQL_CONFIG = {host:'mysqlserver',user:'root',password:'passw0rd',port:3306,database:'zgdata'};
+
+const conn = mysql.createConnection(MYSQL_CONFIG);
+
+conn.connect();
+
+// Get all notes
+app.get('/notes', (req, res) => {
+    
+    try {
+        let search = req.query.search;
+        let query="select title,idntfr from zgprogram where status=0 ";
+        if (search !== "all" && search!==""){
+            query += "and content like '%"+search+"%' or title like '%"+search+"%'";
+        }
+        query+="order by accessdate desc limit 30";
+        console.log(query);
+        conn.query(query,(err,result)=>{
+            if(err) throw err;
+            // console.log(result);
+            res.json(result);
+            //res.json(JSON.parse(data));
+        });
+        
+    } catch (err) {
+        res.send({'error': err.toString()});
+    }
+});
+
+// Get one note details
+app.get('/onenote', (req, res) => {
+    try {
+        let id = req.query.id;
+        let query='';
+        if (id !== ""){
+            // id = id.padStart(10,'0');
+            if(id[0] !== '0') id='0'+id;
+            query="select * from zgprogram where idntfr like '%"+id+"'";
+            console.log(query);
+            conn.query(query,(err,result)=>{
+                if(err) throw err;
+                res.json(result);
+            });
+        }
+    } catch (err) {
+        res.send({'error': err.toString()});
+    }
+})
+// Add new note
+app.put('/onenote', (req, res) => {
+    try {
+        let noteInfo = req.body;
+        let title = '';
+        let content = '';
+        if(noteInfo.title) title=noteInfo.title;
+        if(noteInfo.content) content=noteInfo.content;
+        let query = 'select max(convert(idntfr,unsigned integer)) as max from zgprogram';
+        conn.query(query,(err,result)=>{
+            if(err) throw err;
+            //rt = JSON.parse(result);
+            let max= result[0].max+1;
+            let addnew='insert into zgprogram values("'+title+'","'+content+'","'+max.toString().padStart(10,'0')+'",NOW(),0,NOW())';
+            console.log(addnew);
+            conn.query(addnew,(err,result)=>{
+                if(err) throw err;
+                console.log(result);
+            });
+        });
+        res.send({'success': 'Add new note successfully.'});
+    } catch (err) {
+        res.send({'error': err.toString()});
+    }
+})
+// Update onenote
+app.post('/onenote', (req, res) => {
+    try {
+        let noteInfo = req.body;
+        console.log(noteInfo);
+        let id = req.query.id;
+        let title = '';
+        let content = '';
+        if(noteInfo.title) title=noteInfo.title;
+        if(noteInfo.content) content=noteInfo.content;
+        if (title === '' && content === ''){
+            res.send({'error': 'Title&Content can not be NULL.'});
+        }else{
+            let update='update zgprogram set title="'+title+'",content=\''+content+'\',accessdate=NOW() where idntfr="'+id+'"';
+            console.log(update);
+            conn.query(update,(err,result)=>{
+                if(err) throw err;
+                console.log(result);
+            });
+            res.send({'success': 'Update note successfully.'});
+        }
+    } catch (err) {
+        res.send({'error': err.toString()});
+    }
+});
+// Delete one note
+app.delete('/onenote', (req, res) => {
+    try {
+        let id = req.query.id;
+        let strdelete='update zgprogram set status=-3 where idntfr="'+id+'"';
+        console.log(strdelete);
+        conn.query(strdelete,(err,result)=>{
+            if(err) throw err;
+            console.log(result);
+        });
+        res.send({'success': 'Delete note successfully.'});
+    } catch (err) {
+        res.send({'error': err.toString()});
+    }
+})
+
+const port = 18701
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
